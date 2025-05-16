c7c830b5bda1c441eb60479fd3829a06c235f00b
TeamMiniplays <stefan@miniplays.de>
2025-05-17 01:05:20 +0200

    Default

--- server-counter/src/config/mod.rs
+++ server-counter/src/config/mod.rs
@@ -2,7 +2,10 @@ use serde::Deserialize;
 
 #[derive(Debug, Deserialize)]
 pub struct Config {
+    #[serde(default = "default_server_addr")]
     pub server_addr: String,
+
+    #[serde(default = "default_cache_uri")]
     pub cache_uri: String,
 }
 
@@ -16,4 +19,13 @@ impl Default for Config {
     fn default() -> Self {
         Self::new()
     }
+}
+
+// Default value providers
+fn default_server_addr() -> String {
+    "127.0.0.1:8080".to_string()
+}
+
+fn default_cache_uri() -> String {
+    "postgresql://postgres:null@postgres-cache:5432/botcache".to_string()
 }
