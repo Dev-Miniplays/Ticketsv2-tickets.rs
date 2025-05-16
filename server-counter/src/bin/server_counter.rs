fb526aff3112309ae5d24c0bbe563538123b8b45
TeamMiniplays <stefan@miniplays.de>
2025-05-17 00:31:24 +0200

    test

--- server-counter/src/bin/server_counter.rs
+++ server-counter/src/bin/server_counter.rs
@@ -6,8 +6,11 @@ use server_counter::{http::Server, Config, Error};
 async fn main() -> Result<(), Error> {
     env_logger::init();
 
+    info!("Loading Config...");
     let config = Config::new();
+    info!(config.cache_uri);
 
+    info!("Connecting to Cache...");
     let cache = PostgresCache::connect(config.cache_uri.clone(), cache::Options::default(), 1)
         .await
         .map_err(Error::CacheError)?;
